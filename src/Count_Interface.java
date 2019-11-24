7be8e0325fa86c49ccff64d4732861677c363619
Peyton Williams <peytonwilliams5@gmail.com>
2019-11-24 16:00:59 -0500

    Added documentation

--- src/Count_Interface.java
+++ src/Count_Interface.java
@@ -1,8 +1,42 @@
+/**
+ * Interface for counting meetings
+ *
+ * @author Peyton D Williams will1892@purdue.edu
+ * @version 11/24/2019
+ *
+ */
+
 public interface Count_Interface {
 
     //Will include a data structure to store meeting dates
 
+
+    /**
+     * reads the passed csv file to fill a data structure
+     *
+     * @param file a filepath to csv file
+     *
+     * @return if read was successful
+     */
     boolean read_csv(String file);
+
+    /**
+     * adds the given dates to a data structure
+     *
+     * @param start startdate in the read format
+     * @param end enddate in the read format
+     * @param day_of_week day of the week in read format
+     */
     void add(String start, String end, String day_of_week);
+
+    /**
+     * counts the number of meetings based on passed paramaters
+     *
+     * @param start startdate in the read format
+     * @param end enddate in the read format
+     * @param day_of_week day of the week in read format
+     *
+     * @return the number of meetings between specified dates on day of week
+     */
     int count(String start, String end, String day_of_week);
 }
